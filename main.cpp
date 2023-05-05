bd7e35e04e568916d36cc5c93a6c681528f0e38d
specnazere <gamer39872987z@mail.ru>
2023-05-05 18:37:20 +0700

    init commitr

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Api key: ";
+    string API_KEY;
+    cin >> API_KEY;
+    cout << '\n';
+
+    cout << "steam library path: ";
+    string STEAM_LIBRARY;
+    cin >> STEAM_LIBRARY;
+    cout << '\n';
+}

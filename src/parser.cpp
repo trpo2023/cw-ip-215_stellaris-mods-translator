815c375ef827c24faf47fb8738cf385b231a998d
Yuri Katokhin <kortayn@users.noreply.github.com>
2023-05-05 18:06:02 +0700

    Struct paths and vector of sturcts

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
 #include <filesystem>
+#include <vector>
 
 using namespace std;
 using namespace filesystem;
+
+struct paths
+{
+    string original_path;
+    string translate_path;
+};
 
 string TranslatePath(string path)
 {
     if(path.find("\\english") != string::npos)
         path.replace(path.find("\\english"), 8, "\\russian");
+
     return path.erase(path.rfind("\\"));;
 }
 
-void Parser(path mods_path)
+void Parser(vector<paths> &Localisations, path mods_path)
 {
     for(auto& entry : directory_iterator(mods_path))
     {
-        if(is_directory(entry.status())) Parser(entry.path());
+        if(is_directory(entry.status())) Parser(Localisations, entry.path());
 
         else if(entry.path().string().find("_l_english.yml") != string::npos)
         {
-            cout << entry.path().string() << "\n"
-                 << TranslatePath(entry.path().string()) << "\n";
+            paths localisation_paths;
+            localisation_paths.original_path = entry.path().string();
+            localisation_paths.translate_path = TranslatePath(entry.path().string());
+            Localisations.push_back(localisation_paths);
         }
     }
 }
 
 int main()
 {
+    vector<paths> Localisations;
     path mods_path = "C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\\281990";
 
-    Parser(mods_path);
+    Parser(Localisations, mods_path);
+    
+    for(paths i : Localisations)
+        cout << i.original_path << "\n"
+             << i.translate_path << "\n";
 
     return 0;
 }

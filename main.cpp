6fca98ffebcbfe7c3e133ce12ee488a669db863b
Mikhail Zverev <Alvarise@users.noreply.github.com>
2023-05-20 15:23:21 +0700

    workingType input

--- main.cpp
+++ main.cpp
@@ -14,6 +14,15 @@ int main()
     getline(std::cin, steamLibrary);
     std::cout << '\n';
 
+    std::cout << "Program work type:" << endl
+              << "0 - only make files" << endl
+              << "1 - only translate eng files" << endl
+              << "2 - only retranslate automatically translated" << endl
+              << "3 - replace all" << endl;
+    int workingType;
+    cin >> workingType;
+    cout << endl;
+
     std::vector<paths> Localisations;
     Parser(Localisations, steamLibrary);
 

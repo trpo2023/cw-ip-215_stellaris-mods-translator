e3ea5071e2099719416ec90cb9415f3952e17798
Yuri Katokhin <kortayn@users.noreply.github.com>
2023-05-22 00:07:09 +0700

    Minor improvement

--- src/log.cpp
+++ src/log.cpp
@@ -4,11 +4,11 @@ void log(int code)
 {
     switch(code)
     {
-        case 0: cout << "file is succesfully translated\n"; break;
-        case 1: cout << "file is succesfully transfered\n"; break;
-        case 2: cout << "file is automatically translated before\n"; break;
-        case 3: cout << "file is translated by mod autor\n"; break;
-        case 4: cout << "file already exists\n"; break;
-        case 5: cout << "invalid working type\n"; break;
+        case 0: std::cout << "file is succesfully translated\n"; break;
+        case 1: std::cout << "file is succesfully transfered\n"; break;
+        case 2: std::cout << "file is automatically translated before\n"; break;
+        case 3: std::cout << "file is translated by mod autor\n"; break;
+        case 4: std::cout << "file already exists\n"; break;
+        case 5: std::cout << "invalid working type\n"; break;
     } 
 }
--- src/log.hpp
+++ src/log.hpp
@@ -2,6 +2,4 @@
 
 #include <iostream>
 
-using namespace std;
-
 void log(int code);

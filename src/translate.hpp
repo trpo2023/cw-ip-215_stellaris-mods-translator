4e99c88d7e00cf8dc91718cf26ce9447de4a556f
Yuri Katokhin <kortayn@users.noreply.github.com>
2023-05-23 21:47:46 +0700

    Debug

--- src/translate.hpp
+++ src/translate.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iostream>
-#include <curl/curl.h>
+#include "..\lib\curl-8.1.0_1-win64-mingw\include\curl\curl.h"
 
 size_t WriteCallback(char* buf, size_t size, size_t nmemb, void* userdata);
 std::string translate(std::string str, std::string apiKey);

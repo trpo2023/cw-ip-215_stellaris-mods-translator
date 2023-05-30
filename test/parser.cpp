cf231b7e8b2b3a9f4fb8d18081192776f97b6b39
Yuri Katokhin <kortayn@users.noreply.github.com>
2023-05-30 10:11:43 +0700

    Add translatePath tests

--- test/parser.cpp
+++ test/parser.cpp
@@ -8,3 +8,27 @@ CTEST(directory_tests, parser_test)
     Parser(outputPathsArray, "test/parserTests");
     ASSERT_EQUAL(4, outputPathsArray.size());
 }
+
+CTEST(translate_path, raw_location)
+{
+    std::string raw_location = translatePath("test/parserTests/test_l_english.yml");
+    ASSERT_STR("test/parserTests/test_l_russian.yml", raw_location.c_str());
+}
+
+CTEST(translate_path, language_directory)
+{
+    std::string language_directory = translatePath("test/parserTests/english/test_l_english.yml");
+    ASSERT_STR("test/parserTests/russian/test_l_russian.yml", language_directory.c_str());
+}
+
+CTEST(translate_path, replace_directory)
+{
+    std::string replace_directory = translatePath("test/parserTests/replace/test_l_english.yml");
+    ASSERT_STR("test/parserTests/replace/test_l_russian.yml", replace_directory.c_str());
+}
+
+CTEST(translate_path, replace_language_directory)
+{
+    std::string replace_language_directory = translatePath("test/parserTests/replace/english/test_l_english.yml");
+    ASSERT_STR("test/parserTests/replace/russian/test_l_russian.yml", replace_language_directory.c_str());
+}
